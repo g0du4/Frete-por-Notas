3e5e17271bd72fd587cc01f46e1e67033f04958d
g0du4 <godois98@gmail.com>
2026-09-18 15:42:04 -0300

     

--- bdaivencloud/src/index.js
+++ bdaivencloud/src/index.js
@@ -169,12 +169,12 @@ async function handleNotasFiscais(request, env, ctx) {
        
       const body = await request.json().catch(() => null);
       const id = body?.id;
-      console.log('ID recebido para exclusão:', id);
+      console.log('ID recebido para exclusão:', id.join(', '));
   if (!id) {
     return jsonResponse({ erro: 'ID da nota fiscal é obrigatório' }, 400);
   }
   console.log('ID recebido para exclusão:', 'cmu4v0ufs0002psp7ltb6cb9t');
-  console.log(await prisma.notas_fiscais.find({ where: { id: 'cmu4v0ufs0002psp7ltb6cb9t' }}));
+  await prisma.notas_fiscais.find({ where: { id: cmu4v0ufs0002psp7ltb6cb9t }});
   try {
        await prisma.itens.deleteMany({
       where: { nota_fiscal_id: { in: id } },
