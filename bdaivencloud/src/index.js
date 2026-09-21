4f52cad4a39cbc177b85b4577fa76af0bf7fa16b
g0du4 <godois98@gmail.com>
2026-09-21 19:51:45 -0300

    Retorno de fetch.

--- bdaivencloud/src/index.js
+++ bdaivencloud/src/index.js
@@ -209,8 +209,9 @@ async function handleNotasFiscais(request, env, ctx) {
 					where: { id: { in: id } },
 				});
 
-				
-			} catch (erro) {
+        if(notaDeletada){return jsonResponse({ mensagem: 'Nota fiscal deletada com sucesso', notaDeletada });}
+			
+    } catch (erro) {
 				if (erro.code === 'P2025') {
 					return jsonResponse({ erro: 'Nota fiscal não encontrada' }, 404);
 				}
